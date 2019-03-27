c088717ed02078de1e694987528332e82e073ed2
Tyler Brow <tymuska@yahoo.com>
2019-03-27 08:50:15 -0700

    finished accordian

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var button = $(this)
+     
+        button.on('click', "h2", function(){
+            $('h2').removeClass('down').addClass('up')
+            $(this).addClass('down')
+            
+            
+               
+})})
